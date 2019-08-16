d478d942d077f90f87ab19882966f7bd3b5856a8
zikey <mohsen.xiv@gmail.com>
2019-08-17 02:25:30 +0430

    6

--- app/src/main/java/daryadelan/sandogh/zikey/com/daryadelan/ConfirmCampActivity.java
+++ app/src/main/java/daryadelan/sandogh/zikey/com/daryadelan/ConfirmCampActivity.java
@@ -9,7 +9,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.mohamadamin.persianmaterialdatetimepicker.date.DatePickerDialog;
@@ -29,6 +35,7 @@ public class ConfirmCampActivity extends AppCompatActivity {
     private LinearLayout lyAddPerson;
     private LinearLayout lyDate;
     private LinearLayout lyDayCount;
+    private RelativeLayout lyAddPersonFloat;
 
     private TextView txtDate;
     private TextView txtDayCount;
@@ -36,6 +43,8 @@ public class ConfirmCampActivity extends AppCompatActivity {
     private String date = CalendarWrapper.getCurrentPersianDate();
 
     private int dayCount = 1;
+
+    private ImageView imgDropDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +69,7 @@ public class ConfirmCampActivity extends AppCompatActivity {
     }
 
     private void initClickListeners() {
+
         lyDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,6 +82,40 @@ public class ConfirmCampActivity extends AppCompatActivity {
                 });
             }
         });
+
+        lyDayCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new CustomDialogBuilder().showInputTextDialog_NumbersOnly(ConfirmCampActivity.this, "تعداد روز", new CustomDialogBuilder.OnDialogListener() {
+                    @Override
+                    public void onOK(String input) {
+
+                        dayCount= Integer.parseInt(input);
+                        initContent();
+
+                    }
+
+                    @Override
+                    public void onNeutral(String input) {
+
+                    }
+                });
+            }
+        });
+
+        lyAddPerson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showHideList();
+            }
+        });
+
+        imgDropDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showHideList();
+            }
+        });
     }
 
     private void initViews() {
@@ -82,6 +126,8 @@ public class ConfirmCampActivity extends AppCompatActivity {
 
         txtDate = (TextView) findViewById(R.id.txtDate);
         txtDayCount = (TextView) findViewById(R.id.txtDayCount);
+        lyAddPersonFloat = (RelativeLayout) findViewById(R.id.lyAddPersonFloat);
+        imgDropDown = (ImageView) findViewById(R.id.imgDropDown);
 
         try {
             FontChanger.applyMainFont(findViewById(R.id.lyContent));
@@ -113,5 +159,82 @@ public class ConfirmCampActivity extends AppCompatActivity {
         context.startActivity(starter);
     }
 
+
+    private void showHideList() {
+
+        if (lyAddPersonFloat == null)
+            return;
+        if (lyAddPersonFloat.getVisibility() == View.VISIBLE) {
+            slideDown(lyAddPersonFloat);
+
+        } else {
+            slideUp(lyAddPersonFloat);
+        }
+
+
+    }
+
+    // slide the view from below itself to the current position
+    public void slideUp(View view) {
+
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                lyAddPersonFloat.getHeight(),  // fromYDelta
+                0);                // toYDelta
+        animate.setDuration(500);
+        animate.setFillAfter(true);
+        animate.setInterpolator(new DecelerateInterpolator());
+        animate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        view.startAnimation(animate);
+        lyAddPersonFloat.setVisibility(View.VISIBLE);
+
+    }
+
+    // slide the view from its current position to below itself
+    public void slideDown(final View view) {
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                0,                 // fromYDelta
+                lyAddPersonFloat.getHeight()); // toYDelta
+        animate.setDuration(500);
+        animate.setInterpolator(new AccelerateInterpolator());
+        animate.setFillAfter(true);
+        animate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                lyAddPersonFloat.setVisibility(View.GONE);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        view.startAnimation(animate);
+
+    }
 
 }
