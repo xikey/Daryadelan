c5d722aa76ad88c653612bc7926bb893679de8fd
Zikey <mohsen.xiv@gmail.com>
2020-07-01 10:06:18 +0430

    9

---
+++ app/src/main/java/daryadelan/sandogh/zikey/com/daryadelan/ConversationFooterActivity.java
@@ -0,0 +1,21 @@
+package daryadelan.sandogh.zikey.com.daryadelan;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class ConversationFooterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversation_footer);
+
+    }
+}
